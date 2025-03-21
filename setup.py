977489482ab672ec0afc38fb05d5d2edc9d046f6
Henry Wallace <henry.wallace@rhul.ac.uk>
2025-03-21 11:34:22 +0000

    version bump, make config_manager an optional dependency, bump version

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
+import importlib
 
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+# Function to check if a package is installed
+def is_package_installed(package_name):
+    try:
+        importlib.import_module(package_name)
+        return True
+    except ImportError:
+        return False
+
+# Determine the install_requires list based on whether config-management is installed
+install_requires = [
+    "textual",
+    "textual_dev",
+    "rich",
+]
+
+
 setup(
     name="cider",
-    install_requires=[
-        "textual",
-        "textual_dev",
-        "rich",
-        "config_management @ git+https://gitlab.cern.ch/dune-daq/online/config-management.git", # Config management
-    ],
-    extras_require={"develop": ["ipdb", "ipython"]},
+    install_requires=install_requires,
+    extras_require={"develop": ["ipdb", "ipython"],
+                    "separate_conf": ["config_management @ git+https://gitlab.cern.ch/dune-daq/online/config-management.git"]},
     package_data={"": ["*.tcss", "*.yml"]},
 )
